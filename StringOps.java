7ccca714f97ba640b5f0877046091d26a7b40d98
asafm47 <asaf.manor@post.runi.ac.il>
2024-01-15 10:22:04 +0200

    git ignore

--- StringOps.java
+++ StringOps.java
@@ -22,12 +22,32 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        System.out.println(capVowelsLowRest("Hello World"));
+        System.out.println(capVowelsLowRest("One two tHRee world"));
+        System.out.println(capVowelsLowRest("vowels are fun"));
+        System.out.println(capVowelsLowRest("intro"));
+        System.out.println(capVowelsLowRest("yellow"));
+
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String str = "";
+        for(int i = 0; i< string.length(); i++){
+            char ch = string.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i'|| ch == 'o' || ch == 'u'){
+                ch = ToUpper(ch);
+                str += ch;
+            }
+            else if(ch >= 97 && ch<= 122 || ch>= 65 && ch <=90){
+                ch = ToLower(ch);
+                str += ch;
+            }
+            else{
+                str += ch; 
+            }
+
+        }
+        return str;
     }
 
     public static String camelCase (String string) {
@@ -38,5 +58,21 @@ public class StringOps {
     public static int[] allIndexOf (String string, char chr) {
         // Write your code here:
         return new int[1];
+    }
+
+
+    public static char ToLower(char c){
+        char ch = c;
+        if(ch >='A' && ch <= 'Z'){
+            ch = (char)(ch+32);
+        }
+        return ch;
+    }
+    public static char ToUpper(char c){
+        char ch = c;
+        if(ch >='a' && ch <= 'z'){
+            ch = (char)(ch-32);
+        }
+        return ch;
     }
 }
