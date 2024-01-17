cfb7cdefc81f7576c75ec4e4fbaa569ca49a4539
asafm47 <asaf.manor@post.runi.ac.il>
2024-01-17 18:13:26 +0200

    check1

--- StringOps.java
+++ StringOps.java
@@ -1,78 +1,98 @@
 public class StringOps {
-    ////////////////////////////////////////////////////////////
-    //////                                               ///////
-    //////              Reminder:                        ///////
-    //////        allowed methods                        ///////
-    //////                                               ///////
-    //////        1.charAt(int index)                    ///////
-    //////        2.length()                             ///////
-    //////        3.substring(int start)                 ///////
-    //////        4.substring(int start,int ends)        ///////
-    //////        5.indexOf(String str)                  ///////
-    //////                                               ///////
-    //////        The rest are not allowed !             ///////
-    //////        if you want to use a different         ///////
-    //////        method, and you can implement          ///////
-    //////        it using material from the course      ///////
-    //////        you need to implement a version of     ///////
-    //////        the function by yourself.              ///////
-    //////                                               ///////
-    //////        see example for substring              ///////
-    //////        in Recitation 3 question 5             ///////
-    //////                                               ///////
-    ////////////////////////////////////////////////////////////
+
     public static void main(String[] args) {
-        System.out.println(capVowelsLowRest("Hello World"));
-        System.out.println(capVowelsLowRest("One two tHRee world"));
-        System.out.println(capVowelsLowRest("vowels are fun"));
-        System.out.println(capVowelsLowRest("intro"));
-        System.out.println(capVowelsLowRest("yellow"));
-
-    }
-
-    public static String capVowelsLowRest (String string) {
-        String str = "";
-        for(int i = 0; i< string.length(); i++){
-            char ch = string.charAt(i);
-            if(ch == 'a' || ch == 'e' || ch == 'i'|| ch == 'o' || ch == 'u'){
-                ch = ToUpper(ch);
-                str += ch;
-            }
-            else if(ch >= 97 && ch<= 122 || ch>= 65 && ch <=90){
-                ch = ToLower(ch);
-                str += ch;
-            }
-            else{
-                str += ch; 
-            }
-
+        String word = "MMMM";
+        char chr = 'M';
+        int[] indexes = allIndexOf(word, chr);
+        for (int index : indexes) {
+            System.out.println(index);
         }
-        return str;
     }
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
-    }
-
-    public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
-    }
-
-
-    public static char ToLower(char c){
-        char ch = c;
-        if(ch >='A' && ch <= 'Z'){
-            ch = (char)(ch+32);
+    // Function to count occurrences of chr in a string
+    public static int countOccurrences(String str, char chr) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (chr == str.charAt(i)) {
+                count++;
+            }
         }
-        return ch;
+        return count;
     }
-    public static char ToUpper(char c){
-        char ch = c;
-        if(ch >='a' && ch <= 'z'){
-            ch = (char)(ch-32);
+
+    // Function to get all indexes of chr in string
+    public static int[] allIndexOf(String str, char chr) {
+        int[] indexes = new int[countOccurrences(str, chr)];
+        int currentIndex = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (chr == str.charAt(i)) {
+                indexes[currentIndex++] = i;
+            }
         }
-        return ch;
+        return indexes;
+    }
+
+    // Function to check if a char is lower case
+    public static boolean isLowerCase(char c) {
+        return c >= 'a' && c <= 'z';
+    }
+
+    // Function to check if a char is upper case
+    public static boolean isUpperCase(char c) {
+        return c >= 'A' && c <= 'Z';
+    }
+
+    // Function to convert char to lower case
+    public static char toLowerCase(char c) {
+        return isUpperCase(c) ? (char) (c + 32) : c;
+    }
+
+    // Function to convert char to upper case
+    public static char toUpperCase(char c) {
+        return isLowerCase(c) ? (char) (c - 32) : c;
+    }
+
+    // Function to capitalize vowels and lowercase other chars
+    public static String capVowelsLowRest(String str) {
+        char[] vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        StringBuilder newStr = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            if (indexOf(vowels, currentChar) != -1) {
+                newStr.append(isLowerCase(currentChar) ? toUpperCase(currentChar) : currentChar);
+            } else {
+                newStr.append(isUpperCase(currentChar) ? toLowerCase(currentChar) : currentChar);
+            }
+        }
+        return newStr.toString();
+    }
+
+    // Function to get index of char in array
+    public static int indexOf(char[] array, char c) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == c) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Function to convert string to camel case
+    public static String camelCase(String str) {
+        StringBuilder camelCaseStr = new StringBuilder();
+        boolean newWord = true;
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            if (newWord && isLowerCase(currentChar)) {
+                camelCaseStr.append(toUpperCase(currentChar));
+                newWord = false;
+            } else if (!newWord && isUpperCase(currentChar)) {
+                camelCaseStr.append(toLowerCase(currentChar));
+            } else {
+                camelCaseStr.append(currentChar);
+                newWord = (currentChar == ' ');
+            }
+        }
+        return camelCaseStr.toString();
     }
 }
